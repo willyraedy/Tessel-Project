e1ebc34c93a53dddcecb75dcaef6e30b53d6fa68
Kevin Lobo <kevinlobo91@gmail.com>
2017-06-26 17:11:24 -0500

    final

--- gps.js
+++ gps.js
@@ -10,33 +10,46 @@ var gpsLib = require('gps-a2235h');
 var getDistance = require('./distance.js')
 var getDestination = require('./getDestination.js')
 var gps = gpsLib.use(tessel.port['A']);
+var getSearch = require('./getSearch.js')
+
+
 // Wait until the module is connected
-gps.on('ready', function () {
+gps.on('ready', function() {
   console.log('GPS module powered and ready. Waiting for satellites...');
   // Emit coordinates when we get a coordinate fix
-  gps.on('coordinates', function (coords) {
-    console.log('hi')
+  gps.on('coordinates', function(coords) {
+
     try {
-    var reducedLat = coords.lat[0] + coords.lat[1] / 60
-    var reducedLng = coords.lon[0] + coords.lon[1] / 60
-  }
-  catch(err) {}
-    console.log('reducedlat: ', reducedLat, " reducedlng: ",reducedLng)
+      var reducedLat = coords.lat[0] + coords.lat[1] / 60;
+      var reducedLng = coords.lon[0] + coords.lon[1] / 60;
+    } catch (err) {}
+    //console.log('reducedlat: ', reducedLat, " reducedlng: ",reducedLng)
     // obj with lat key and lng key 41.89 and 87.637
-    var dest = getDestination('merchandise mart brown line station')
-    var dist = getDistance({lat: reducedLat, lng: reducedLng},{lat: dest.lat, lng: dest.lng})
-    // where we check things!!!!!!!!!!!!!!!!!!
-    console.log(dist)
-    if (dist<.5){
-    setInterval(function () {
-      tessel.led[2].toggle()
-      tessel.led[3].toggle()
-    },100)
-    }
+    getSearch
+      .then(function(stop) {
+          return getDestination(stop);
+        })
+        .then(function(dest){
+          var dist = getDistance({
+            lat: reducedLat,
+            lng: reducedLng
+          }, {
+            lat: dest.lat,
+            lng: dest.lng
+          });
+
+          if (dist < 0.5){
+            setInterval(function() {
+              tessel.led[2].toggle();
+              tessel.led[3].toggle();
+            }, 100);
+          }
+        });
+
     console.log('Lat:', coords.lat, '\tLon:', coords.lon, '\tTimestamp:', coords.timestamp);
   });
   // Emit altitude when we get an altitude fix
-  gps.on('altitude', function (alt) {
+  gps.on('altitude', function(alt) {
     // test blinker
     // if (true){
     // setInterval(function () {
@@ -50,11 +63,11 @@ gps.on('ready', function () {
   // gps.on('fix', function (data) {
   //   console.log(data.numSat, 'fixed.');
   // });
-  gps.on('dropped', function(){
+  gps.on('dropped', function() {
     // we dropped the gps signal
     console.log("gps signal dropped");
   });
 });
-gps.on('error', function(err){
+gps.on('error', function(err) {
   console.log("got this error", err);
 });
