8be7ec61a6e6b2b248b938131962b492c9149bf1
Kevin Lobo <kevinlobo91@gmail.com>
2017-06-26 15:31:16 -0500

    distance.js & test.js for for dis & wifi coordinates added

--- gps.js
+++ gps.js
@@ -17,18 +17,39 @@ gps.on('ready', function () {
   console.log('GPS module powered and ready. Waiting for satellites...');
   // Emit coordinates when we get a coordinate fix
   gps.on('coordinates', function (coords) {
+
+    var reducedLat = coords.lat[0] + coords.lat[1] / 60
+    var reducedLong = coords.lon[0] + coords.lon[1] / 60
+
+
+    // where we check things!!!!!!!!!!!!!!!!!!
+    if (true){
+    setInterval(function () {
+      tessel.led[2].toggle()
+      tessel.led[3].toggle()
+    },100)
+    }
     console.log('Lat:', coords.lat, '\tLon:', coords.lon, '\tTimestamp:', coords.timestamp);
   });
 
   // Emit altitude when we get an altitude fix
   gps.on('altitude', function (alt) {
+
+    // test blinker
+    // if (true){
+    // setInterval(function () {
+    //   tessel.led[2].toggle()
+    //   tessel.led[3].toggle()
+    // },100)
+    // }
+
     console.log('Got an altitude of', alt.alt, 'meters (timestamp: ' + alt.timestamp + ')');
   });
 
   // Emitted when we have information about a fix on satellites
-  gps.on('fix', function (data) {
-    console.log(data.numSat, 'fixed.');
-  });
+  // gps.on('fix', function (data) {
+  //   console.log(data.numSat, 'fixed.');
+  // });
 
   gps.on('dropped', function(){
     // we dropped the gps signal
